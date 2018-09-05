7555590bcaa8ebcef702897251f796e204177030
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2018-09-05 23:15:41 +0300

    Sass imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Header } from './Header';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import { Background } from './Background';
 import { Footer } from './Footer';
 import { Main } from './Main';
-
+import './App.css'
 class App extends Component {
   render() {
     return (
--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { MainPageLogo } from '../MainPageLogo';
-import './header.css'
 
 class Header extends Component {
     render() {
